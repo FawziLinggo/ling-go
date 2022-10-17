68a2f5c92e4605237320b723e6b1ba88e459e700
MIB <bobby@mitraintibersama.com>
2022-10-17 07:38:13 +0700

    create implement

---
+++ 2.restful-api-go/service/category_service_impl.go
@@ -0,0 +1,88 @@
+package service
+
+import (
+	"FawziLinggo/ling-go/2.restful-api-go/helper"
+	"FawziLinggo/ling-go/2.restful-api-go/model/domain"
+	"FawziLinggo/ling-go/2.restful-api-go/model/web"
+	"FawziLinggo/ling-go/2.restful-api-go/repository"
+	"context"
+	"database/sql"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type CategoryServiceImpl struct {
+	CategoryRepository repository.CategoryReposiroty
+	DB                 *sql.DB
+	Validate           *validator.Validate
+}
+
+func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	// Validasi
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category := domain.Category{
+		Name: request.Name,
+	}
+
+	category = service.CategoryRepository.Save(ctx, tx, category)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	// Validasi
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, request.Id)
+	helper.PanicIfError(err)
+
+	category.Name = request.Name
+	category = service.CategoryRepository.Update(ctx, tx, category)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Delete(ctx context.Context, categoryId int) {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+
+	service.CategoryRepository.Delete(ctx, tx, category)
+}
+
+func (service *CategoryServiceImpl) FindById(ctx context.Context, categoryId int) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	categories := service.CategoryRepository.FindAll(ctx, tx)
+
+	var categoriesResponses []web.CategoryResponse
+	for _, category := range categories {
+		categoriesResponses = append(categoriesResponses, helper.ToCategoryResponse(category))
+	}
+	return categoriesResponses
+}
